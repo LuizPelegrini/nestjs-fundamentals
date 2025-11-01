50bc3b8a21e809fda0fdfd0fef4238fd1fffe0aa
LuizPelegrini <luiz.guilherme@stashaway.com>
2025-11-01 16:23:01 +0800

    add whitelist setting to validation pipe

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // strips out any props in request body that don't exist in DTO
+      forbidNonWhitelisted: true, // throw errors when props that don't exist in DTO are included in the request
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
