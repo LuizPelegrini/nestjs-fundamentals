99fa540666ffa77238abd9146ce281ac43a36e0a
LuizPelegrini <luiz.guilherme@stashaway.com>
2025-11-22 15:51:42 +0800

    adds postgres integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin123',
+      database: 'postgres',
+      // creates DB tables from Entity classes automatically. It doesn't require migrations to run
+      autoLoadEntities: true,
+      // Setting synchronize: true -> Indicates if database schema should be auto created on every application launch
+      // it shouldn't be used in production - otherwise you can lose production data.
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
